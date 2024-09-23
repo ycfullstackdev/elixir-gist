21a188f74d4d7b0be21ca9ddf787a2c9f2b8e114
yc <yc@ycfullstack.com>
2024-09-22 19:47:33 -0600

    feat: fix css
    Signed-off-by: yc <yc@ycfullstack.com>

--- lib/elixir_gist_web/components/core_components.ex
+++ lib/elixir_gist_web/components/core_components.ex
@@ -281,6 +281,10 @@ defmodule ElixirGistWeb.CoreComponents do
   attr :field, Phoenix.HTML.FormField,
     doc: "a form field struct retrieved from the form, for example: @form[:email]"
 
+  attr :class, :string,
+    default:
+      "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6 min-h-[6rem]"
+
   attr :errors, :list, default: []
   attr :checked, :boolean, doc: "the checked flag for checkbox inputs"
   attr :prompt, :string, default: nil, doc: "the prompt for select inputs"
@@ -355,7 +359,7 @@ defmodule ElixirGistWeb.CoreComponents do
         id={@id}
         name={@name}
         class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6 min-h-[6rem]",
+          @class,
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
           @errors != [] && "border-rose-400 focus:border-rose-400"
         ]}
